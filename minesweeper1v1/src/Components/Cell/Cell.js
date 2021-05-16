5222feea15d795168f154c7eb767eb9f2653f718
Rahul R S <rahulrsgoku@gmail.com>
2021-05-17 04:23:19 +0530

    Cell Components update

--- minesweeper1v1/src/Components/Cell/Cell.js
+++ minesweeper1v1/src/Components/Cell/Cell.js
@@ -1,9 +1,37 @@
-import react from 'react'
+import React from 'react';
+/*import two from '../../assets/2.png';
+import three from '../../assets/3.png';
+import four from '../../assets/4.png';
+import five from '../../assets/5.png';
+import six from '../../assets/6.png';
+import seven from '../../assets/7.png';
+import eight from '../../assets/8.png';
+import mine from '../../assets/m.png';
+import flag from '../../assets/f.png';
+import normal from '../../assets/x.png';*/
+import classes from './Cell.module.css';
 
-const cell = (props) =>{
-    return (<div>
 
-    </div>)
+const cell = (props)=>
+{
+    const style = {
+        block: {
+          width: 40,
+          height: 40,
+          color: 'red',
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontWeight: 800,
+          fontSize: 30,
+          cursor: "pointer",
+          backgroundColor:'green'
+      }};
+ return (
+ <div  style={style.block} >
+    
+ </div>)
+
 }
 
 export default cell;
